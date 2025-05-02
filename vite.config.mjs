67fac50c503a7f1b474dc9a11ffbed1ca24af294
Yemen Dickinson <48459727+sirkel360@users.noreply.github.com>
2025-05-01 17:09:51 -0700

    Add files via upload

---
+++ vite.config.mjs
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      'react-native$': 'react-native-web'
+    }
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      clientPort: 443
+    },
+    cors: true,
+    allowedHosts: 'all'
+  }
+});
